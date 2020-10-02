1f2ad4edf3af85eb086eb5870ec1da0de1091a3c
hmisonne <helene.misonne@gmail.com>
2020-10-02 09:43:16 -0700

    feat: Added load and remove products

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,3 +2,13 @@ export const addProduct = (product) => ({
     type: "ADD_PRODUCT",
     product,
 });
+
+export const deleteProduct = (id) => ({
+    type: "DELETE_PRODUCT",
+    id,
+});
+
+export const loadProducts = (products) => ({
+    type: "LOAD_PRODUCTS",
+    products,
+});
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,14 +3,19 @@ import _ from 'lodash';
 
 export const ProductReducer = (state = [], action) => {
     switch(action.type){
+        case "LOAD_PRODUCTS":
+            const products = action.products
+            return products
         case "ADD_PRODUCT":
             const newProduct = action.product
             return [...state, newProduct]
+        case "DELETE_PRODUCT":
+            return state.filter(product => (product.id !== action.id))
         default:
             return state;
     }
 }
 
 export default combineReducers({
-    product: ProductReducer,
+    products: ProductReducer,
 });

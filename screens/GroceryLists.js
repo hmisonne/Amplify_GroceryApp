23d86cb14f53a8a4b296425fc0558afbe22d2991
hmisonne <helene.misonne@gmail.com>
2020-10-06 15:30:31 -0700

    feat: Added goToList nav

--- screens/GroceryLists.js
+++ screens/GroceryLists.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import {
-    View, Text, StyleSheet, Button, Switch, TouchableOpacity
+    View, Text, StyleSheet, TouchableOpacity
   } from 'react-native'
 import { connect, useDispatch } from 'react-redux'
 import { loadGroceryLists} from '../src/redux/actions/groceryList'
@@ -8,7 +8,6 @@ import { DataStore } from "@aws-amplify/datastore";
 import { GroceryList } from '../src/models'
 import store from '../src/redux/store';
 
-import { AntDesign } from '@expo/vector-icons';
 
 const GroceryLists = (props) => {
     const dispatch = useDispatch()
@@ -17,8 +16,10 @@ const GroceryLists = (props) => {
         fetchLists();
     }, [])
     
-    
-    
+    function goToList(groceryListID){
+        // const products = groceryLists.filter(list => list.id === groceryListID)[0]
+        return props.navigation.push('ProductCategory',{ groceryListID})
+    }
     async function fetchLists() {
     try {
         const data = await DataStore.query(GroceryList);
@@ -35,11 +36,14 @@ const GroceryLists = (props) => {
     <View style={styles.container}>
     {
         groceryLists.map((product, index) => (
-          <View key={product.id ? product.id : index} style={styles.product}>
+          <TouchableOpacity 
+            key={product.id ? product.id : index} 
+            style={styles.product}
+            onPress={() => goToList(product.id)}>
             <View style={styles.subContainer}>
               <Text style={styles.productName}>{product.name}</Text>
             </View>
-          </View>
+          </TouchableOpacity>
         ))
       }
       </View>

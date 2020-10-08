a8532521c6f75c1c11f94b37422be05e12a02d8a
hmisonne <helene.misonne@gmail.com>
2020-10-08 13:22:08 -0700

    feat: Added User instance upon connection

--- screens/Home.js
+++ screens/Home.js
@@ -4,7 +4,8 @@ import { useDispatch } from 'react-redux'
 import SubmitBtn from '../components/SubmitBtn'
 import { authentificateUser } from '../src/redux/actions/user'
 import { Auth } from 'aws-amplify'
-
+import { DataStore, Predicates } from "@aws-amplify/datastore";
+import { User } from '../src/models'
 
 const Home = (props) => {
     const dispatch = useDispatch()
@@ -21,6 +22,23 @@ const Home = (props) => {
     async function identifyUser() {
         try {
             const userInfo = await Auth.currentUserInfo()
+
+            // let users = await DataStore.query(User, c => c.sub("eq", userInfo.attributes.sub));
+            let users = await DataStore.query(User);
+
+            console.log('users', users, 'userInfo')
+            if (users.length === 0){
+                const user = await DataStore.save(
+                    new User({
+                        id: userInfo.id,
+                        name: userInfo.username,
+                        email: userInfo.attributes.email,
+                        // sub: userInfo.attributes.sub
+                    })
+                  )
+                  console.log('user',user)
+            }
+            
             dispatch(authentificateUser(userInfo))
             console.log("User info retrieved successfully!");
         } catch (error) {

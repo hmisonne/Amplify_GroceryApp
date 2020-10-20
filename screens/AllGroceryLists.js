56e9ecc434b348dc0d8fb62a8d4293af7a181d55
hmisonne <helene.misonne@gmail.com>
2020-10-19 17:02:26 -0700

    refactor: Moved function fetch grocerylist to api file

--- screens/AllGroceryLists.js
+++ screens/AllGroceryLists.js
@@ -1,48 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-import { connect } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 import { DataStore } from "@aws-amplify/datastore";
 import { User, GroceryList } from "../src/models";
 import store from "../src/redux/store";
+import { addGroceryListToUser, fetchAllGroceryLists } from '../utils/api'
 
 const AllGroceryLists = (props) => {
   const { user } = store.getState();
   const [groceryListsState, setGlistState] = useState([]);
-
+  const dispatch = useDispatch();
   useEffect(() => {
-    fetchLists();
+    fetchAllGroceryLists(setGlistState);
   }, []);
 
-  async function addListToUser(groceryListID) {
-    const currentUser = await DataStore.query(User, (c) =>
-      c.sub("eq", user.sub)
-    );
 
-    await DataStore.save(
-      User.copyOf(currentUser[0], (updated) => {
-        updated.userGroceryListID = updated.userGroceryListID
-          ? [...updated.userGroceryListID, groceryListID]
-          : [groceryListID];
-      })
-    );
-  }
 
-  async function fetchLists() {
-    try {
-      const allGroceryLists = await DataStore.query(GroceryList);
-      setGlistState(allGroceryLists);
-      console.log("grocery lists retrieved successfully!");
-    } catch (error) {
-      console.log("Error retrieving grocery lists", error);
-    }
-  }
 
   return (
     <View style={styles.container}>
       {groceryListsState.map((list, index) => (
         <View key={list.id ? list.id : index} style={styles.list}>
           <Text style={styles.listName}>{list.name}</Text>
-          <Button title="Add" onPress={() => addListToUser(list.id)} disabled={user.userGroceryListID && user.userGroceryListID.includes(list.id)} />
+          <Button title="Add" onPress={() => addGroceryListToUser(list.id, user, dispatch)} disabled={user.userGroceryListID && user.userGroceryListID.includes(list.id)} />
         </View>
       ))}
     </View>

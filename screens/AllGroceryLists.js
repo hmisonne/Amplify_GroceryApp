fb5093e82bf9c8a41a371f7fd723346f376a64fa
hmisonne <helene.misonne@gmail.com>
2020-10-20 17:02:01 -0700

    refactor: Removed dispatch from function addGroceryList

--- screens/AllGroceryLists.js
+++ screens/AllGroceryLists.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { connect, useDispatch } from "react-redux";
-import { DataStore } from "@aws-amplify/datastore";
-import { User, GroceryList } from "../src/models";
 import store from "../src/redux/store";
 import { addGroceryListToUser, fetchAllGroceryLists } from '../utils/api'
 
 const AllGroceryLists = (props) => {
   const { user } = store.getState();
   const [groceryListsState, setGlistState] = useState([]);
-  const dispatch = useDispatch();
   useEffect(() => {
     fetchAllGroceryLists(setGlistState);
   }, []);
@@ -22,7 +19,7 @@ const AllGroceryLists = (props) => {
       {groceryListsState.map((list, index) => (
         <View key={list.id ? list.id : index} style={styles.list}>
           <Text style={styles.listName}>{list.name}</Text>
-          <Button title="Add" onPress={() => addGroceryListToUser(list.id, user, dispatch)} disabled={user.userGroceryListID && user.userGroceryListID.includes(list.id)} />
+          <Button title="Add" onPress={() => addGroceryListToUser(list.id, user)} />
         </View>
       ))}
     </View>

d38961ae2d6b57d1aca46cba0599dd877ceb2b8b
hmisonne <helene.misonne@gmail.com>
2020-10-21 11:30:54 -0700

    feat: Added Scrollview for mobile

--- screens/ProductList.js
+++ screens/ProductList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Switch, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Switch, TouchableOpacity, Platform, ScrollView } from "react-native";
 import { connect, useDispatch } from "react-redux";
 import { deleteProduct, toggleProduct } from "../src/redux/actions/product";
 import { DataStore } from "@aws-amplify/datastore";
@@ -42,35 +42,47 @@ const ProductList = (props) => {
     return props.navigation.push("AddProduct", { product });
   }
 
-  return (
-    <View style={styles.container}>
-      {productsByCat.map((product, index) => (
-        <View key={product.id ? product.id : index} style={styles.product}>
-          <View style={styles.subContainer}>
-            <Switch
-              value={product.checked}
-              onValueChange={() => onToggle(product.id)}
+  function showProductList() {
+    return (
+      <View style={styles.container}>
+        {productsByCat.map((product, index) => (
+          <View key={product.id ? product.id : index} style={styles.product}>
+            <View style={styles.subContainer}>
+              <Switch
+                value={product.checked}
+                onValueChange={() => onToggle(product.id)}
+              />
+              <TouchableOpacity style={styles.textAndQty} onPress={() => goToEditProduct(product)}>
+                <View>
+                  <Text style={styles.productName}>{product.name}</Text>
+                </View>
+                <View>
+                  <Text style={styles.productName}>
+                  {product.quantity} {product.unit}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <RoundButton
+              onPress={() => removeProduct(product.id)}
+              name="minus-circle"
+              color="red"
             />
-            <TouchableOpacity style={styles.textAndQty} onPress={() => goToEditProduct(product)}>
-              <View>
-                <Text style={styles.productName}>{product.name}</Text>
-              </View>
-              <View>
-                <Text style={styles.productName}>
-                {product.quantity} {product.unit}
-                </Text>
-              </View>
-            </TouchableOpacity>
           </View>
-          <RoundButton
-            onPress={() => removeProduct(product.id)}
-            name="minus-circle"
-            color="red"
-          />
-        </View>
-      ))}
+        ))}
+      </View>
+    );
+  }
+  return (
+    <View>
+      {Platform.OS !== "ios" && Platform.OS !== "android" ? (
+        <View>{showProductList()}</View>
+      ) : (
+        <ScrollView>{showProductList()}</ScrollView>
+      )}
     </View>
   );
+  
 };
 const mapStateToProps = (state) => ({
   products: state.products,

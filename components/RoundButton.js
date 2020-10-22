542a73dbd9d057fce9dfbdf31e854fd5a1207b94
hmisonne <helene.misonne@gmail.com>
2020-10-22 14:33:52 -0700

    style: Added default size for button

--- components/RoundButton.js
+++ components/RoundButton.js
@@ -2,11 +2,11 @@ import React from "react";
 import { View, TouchableOpacity } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function RoundButton({ onPress, name, color, style = {} }) {
+export default function RoundButton({ onPress, name, color, style = {}, size=24 }) {
   return (
     <View>
       <TouchableOpacity style={style} onPress={onPress}>
-        <MaterialCommunityIcons name={name} size={24} color={color} />
+        <MaterialCommunityIcons name={name} size={size} color={color} />
       </TouchableOpacity>
     </View>
   );

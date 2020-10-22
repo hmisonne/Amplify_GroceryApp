e2803990e913bca6196260d7dbffae919b4784be
hmisonne <helene.misonne@gmail.com>
2020-10-22 10:43:09 -0700

    refactor: Moved User Lists to Home Screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,19 @@
 import React, { useEffect } from "react";
-import { View, StyleSheet } from "react-native";
-import { useDispatch } from "react-redux";
+// import { View, StyleSheet } from "react-native";
+// import { useDispatch } from "react-redux";
 import SubmitBtn from "../components/SubmitBtn";
 import { identifyUser } from '../utils/api'
-
+import { connect, useDispatch } from "react-redux";
+import store from "../src/redux/store";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import RoundButton from "../components/RoundButton";
+import { removeGroceryListFromUser, fetchUserGroceryLists } from '../utils/api'
 
 const Home = (props) => {
   const dispatch = useDispatch();
+  const { groceryLists, user } = store.getState();
   useEffect(() => {
-    identifyUser(dispatch);
+    loadResources()
     // const subscription = DataStore.observe(User).subscribe((msg) => {
     //   console.log("sync users", msg.model, msg.opType, msg.element);
     //   identifyUser(dispatch);
@@ -17,8 +22,11 @@ const Home = (props) => {
     // return () => subscription.unsubscribe();
   }, []);
 
-
-
+  
+  async function loadResources(){
+    const currUser = await identifyUser(dispatch);
+    fetchUserGroceryLists(dispatch, currUser);
+  }
 
   function goToAllGroceryList() {
     return props.navigation.push("AllGroceryLists");
@@ -27,25 +35,75 @@ const Home = (props) => {
     return props.navigation.push("NewList");
   }
 
-  function goToSavedGroceryList() {
-    return props.navigation.push("GroceryLists");
-  }
 
   return (
     <View style={styles.container}>
-      <SubmitBtn title="Saved List(s)" onPress={goToSavedGroceryList} />
-      <SubmitBtn title="New List" onPress={goToNewGroceryList} />
-      <SubmitBtn title="Browse Lists" onPress={goToAllGroceryList} />
+      {displayUserGroceryLists()}
+      <RoundButton
+              onPress={() => goToNewGroceryList()}
+              name="plus-circle"
+              color="blue"
+              size={40}
+              style={styles.bottom}
+            />
     </View>
   );
+
+  function goToList(groceryList) {
+    return props.navigation.push("ProductCategory", { groceryList });
+  }
+  
+  function displayUserGroceryLists() {
+    return (
+      <View style={styles.container}>
+        {groceryLists.map((glist, index) => (
+          <View style={styles.glist} key={glist.id ? glist.id : index}>
+            <TouchableOpacity
+              style={styles.product}
+              onPress={() => goToList(glist)}
+            >
+              <View style={styles.subContainer}>
+                <Text style={styles.glistName}>{glist.name}</Text>
+              </View>
+            </TouchableOpacity>
+            <RoundButton
+              onPress={() => removeGroceryListFromUser(glist.id, user, dispatch)}
+              name="minus-circle"
+              color="red"
+              
+            />
+          </View>
+        ))}
+      </View>
+    );
+  }
+  
 };
 
-export default Home;
+const mapStateToProps = (state) => ({
+  groceryLists: state.groceryLists,
+  user: state.user
+});
+
+export default connect(mapStateToProps)(Home);
+// export default Home;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-around",
     padding: 20,
   },
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems:'flex-end',
+    marginBottom: 36
+  }
 });
+
+{/* <View style={styles.container}>
+      
+      <SubmitBtn title="Saved List(s)" onPress={goToSavedGroceryList} />
+      <SubmitBtn title="New List" onPress={goToNewGroceryList} />
+      <SubmitBtn title="Browse Lists" onPress={goToAllGroceryList} />
+    </View> */}

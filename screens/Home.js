331d7144bddfeeb0003b783f8afda6a57231a2db
hmisonne <helene.misonne@gmail.com>
2020-10-23 12:31:44 -0700

    refactor: Added instruction text if no grocery list have been added

--- screens/Home.js
+++ screens/Home.js
@@ -3,7 +3,7 @@ import SubmitBtn from "../components/SubmitBtn";
 import { identifyUser } from "../utils/api";
 import { connect, useDispatch } from "react-redux";
 import store from "../src/redux/store";
-import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import RoundButton from "../components/RoundButton";
 import { removeGroceryListFromUser, fetchUserGroceryLists } from "../utils/api";
 import { blue } from "../utils/colors";
@@ -35,7 +35,10 @@ const Home = (props) => {
 
   return (
     <View style={styles.container}>
-      {displayUserGroceryLists()}
+      {groceryLists.length ===0 ?
+        displayInstructions()
+        :displayUserGroceryLists()
+      }
       <RoundButton
         onPress={() => goToNewGroceryList()}
         name="plus-circle"
@@ -55,6 +58,13 @@ const Home = (props) => {
 
   function goToList(groceryList) {
     return props.navigation.push("ProductCategory", { groceryList });
+  }
+  function displayInstructions() {
+    return (
+      <View style={styles.centered}>
+        <Text style={styles.text}> Create your first grocery list by clicking on the + icon or browse existing lists by pressing the Search icon !</Text>
+      </View>
+    );
   }
 
   function displayUserGroceryLists() {
@@ -93,6 +103,14 @@ export default connect(mapStateToProps)(Home);
 // export default Home;
 
 const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    textAlign: "center"
+  },
   container: {
     flex: 1,
     padding: 20,

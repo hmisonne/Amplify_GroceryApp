1b9876ec4b148e6145c3aed241fffabadd0c01b1
hmisonne <helene.misonne@gmail.com>
2020-11-02 19:00:38 -0800

    feat: Added option to share a groceryList by email

--- screens/ShareGroceryList.js
+++ screens/ShareGroceryList.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import * as MailComposer from 'expo-mail-composer';
 import SubmitBtn from "../components/SubmitBtn";
+import { Auth } from "aws-amplify";
 
+     
 const ShareGroceryList = (props) => {
   const { groceryListID } = props.route.params;
-  function copyToClipBoard() {
-    console.log(groceryListID);
+
+  const mailComposerOptions = {
+    subject: `ListBee: Someone wants to share a Grocery List with you !`,
+    body: `Welcome to ListBee! 
+    
+    If you don't have the app go to the Android App Store:
+    https://play.google.com/store/apps/details?id=com.hmisonne.ListBee, 
+    
+    Or go to the website: https://master.d22bl963x1qfmv.amplifyapp.com/
+    
+    After signing-up, click on the + button to join a Grocery List and paste this number:
+    ${groceryListID}`
+  }
+  function shareList() {
+    MailComposer.composeAsync(mailComposerOptions)
   }
   return (
     <View style={styles.container}>
       <View>
       <View>
-        <Text style={styles.text}>Send this reference to your friend :</Text>
+        <Text style={styles.text}> To share your list, use this reference :</Text>
       </View>
       <View>
         <Text style={styles.text}>{groceryListID}</Text>
       </View>
       </View>
+      <SubmitBtn title="Send an email" onPress={shareList}/>
       
-
-      <SubmitBtn title="Share" onPress={copyToClipBoard} />
     </View>
   );
 };

5d21711dabbde808606772a2ddc8f80397c62059
hmisonne <helene.misonne@gmail.com>
2020-11-04 10:03:02 -0800

    style: Updated email content and style of component

--- screens/ShareGroceryList.js
+++ screens/ShareGroceryList.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
-import * as MailComposer from 'expo-mail-composer';
+import * as MailComposer from "expo-mail-composer";
 import SubmitBtn from "../components/SubmitBtn";
 import { connect } from "react-redux";
-     
+
 const ShareGroceryList = (props) => {
   const { user } = props;
-  
   const { groceryListID } = props.route.params;
 
   const mailComposerOptions = {
     isHtml: true,
     subject: `ListBee: ${user.username} wants to share a Grocery List with you !`,
-    body: `<div> Welcome to ListBee!</div> 
+    body: `Welcome to ListBee! 
     
     If you don't have the app go to the Android App Store:
     https://play.google.com/store/apps/details?id=com.hmisonne.ListBee, 
@@ -20,23 +19,20 @@ const ShareGroceryList = (props) => {
     Or go to the website: https://master.d22bl963x1qfmv.amplifyapp.com/
     
     After signing-up, click on the + button to join a Grocery List and paste this number:
-    ${groceryListID}`
-  }
+
+    ${groceryListID}
+    
+    You are ready to start shopping!
+    `,
+  };
   function shareList() {
-    MailComposer.composeAsync(mailComposerOptions)
+    MailComposer.composeAsync(mailComposerOptions);
   }
   return (
     <View style={styles.container}>
-      <View>
-      <View>
-        <Text style={styles.text}> To share your list, use this reference :</Text>
-      </View>
-      <View>
-        <Text style={styles.text}>{groceryListID}</Text>
-      </View>
-      </View>
-      <SubmitBtn title="Send an email" onPress={shareList}/>
-      
+      <Text style={styles.text}> To share your list, use this reference :</Text>
+      <Text style={styles.ref}>{groceryListID}</Text>
+      <SubmitBtn title="Send an email" onPress={shareList} />
     </View>
   );
 };
@@ -54,5 +50,9 @@ const styles = StyleSheet.create({
   },
   text: {
     textAlign: "center",
+  },
+  ref: {
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });

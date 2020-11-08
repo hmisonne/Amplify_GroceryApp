274d9389f46e475f4378db88f092f2875341d89d
hmisonne <helene.misonne@gmail.com>
2020-11-08 07:15:30 -0800

    refactor: Updated signUp config fields

--- App.js
+++ App.js
@@ -146,4 +146,33 @@ const App = () => {
   );
 };
 
-export default withAuthenticator(App);
+const signUpConfig = {
+  header: 'Sign Up',
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: 'Username:',
+      key: 'username',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Password (8 characters min):',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password'
+    },
+    {
+      label: 'Email:',
+      key: 'email',
+      required: true,
+      displayOrder: 3,
+      type: 'string'
+    }
+  ]
+};
+
+export default withAuthenticator(App, { signUpConfig });

bbae43949656c268300ae75d472b884d5b17f3aa
hmisonne <helene.misonne@gmail.com>
2020-11-09 16:45:02 -0800

    style: Updated color

--- components/LoadingCircle.js
+++ components/LoadingCircle.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { ActivityIndicator, Colors } from 'react-native-paper';
+import { mainColor } from '../utils/helpers';
 
 const LoadingCircle = () => (
-  <ActivityIndicator animating={true} color={Colors.white} size='large'/>
+  <ActivityIndicator animating={true} color={mainColor} size='large'/>
 );
 
 export default LoadingCircle;

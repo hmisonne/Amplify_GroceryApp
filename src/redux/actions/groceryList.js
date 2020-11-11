9c9389ce1f611880d9ec8daef2b4e70210f93b88
hmisonne <helene.misonne@gmail.com>
2020-11-11 12:13:13 -0800

    feat: Added syncDatastore function and moved selectiveSync from api file to current file

--- src/redux/actions/groceryList.js
+++ src/redux/actions/groceryList.js
@@ -1,4 +1,26 @@
 import { API } from "../../../utils/api";
+import { DataStore, syncExpression } from 'aws-amplify'
+import {  GroceryList, Product } from "../../models";
+
+
+// let groceryListIDs = ['1b2c6e03-241a-4205-b884-5dfe132b526d','306ac6f3-ea62-4d5f-aee7-f7112d716db3']
+let groceryListIDs = ['']
+
+const listPredicate = (c, list, field) => {
+  console.log('groceryListIDs',groceryListIDs)
+  return c.or((c) => list.reduce((_c, operand) => _c[field]('eq', operand), c));
+};
+
+DataStore.configure({
+  syncExpressions: [
+    syncExpression(Product, () => {
+      return (c) => listPredicate(c, groceryListIDs, 'groceryListID');
+    }),
+    syncExpression(GroceryList, () => {
+      return (c) => listPredicate(c, groceryListIDs, 'id');
+    }),
+  ],
+});
 
 export const addGroceryList = (groceryList) => ({
   type: "ADD_GROCERY_LIST",
@@ -32,10 +54,10 @@ export function handleDeleteGroceryList(id) {
 	}
 }
 
-export function handleAddGroceryList(id) {
+export function handleAddGroceryList() {
   return (dispatch, getState) => {
 		const {user} = getState()
-		return API.addGroceryListToUser(id, user)
+		return API.addGroceryListToUser()
 			.then((groceryList)=> dispatch(addGroceryList(groceryList)))
 	}
 }
@@ -47,3 +69,37 @@ export function handleCreateGroceryList(groceryList) {
 			.then((groceryList)=> dispatch(addGroceryList(groceryList)))
 	}
 }
+
+
+export async function syncDatastore(id, action){
+	console.log('sync DS', id, action)
+	switch (action){
+		case "ADD":
+			console.log('ADD')
+			groceryListIDs[0] === '' ?
+			groceryListIDs[0] = id
+			: groceryListIDs.push(id)
+			return restartDataStore()
+		case "REMOVE":
+			console.log('REMOVE')
+			if (groceryListIDs.includes(id)){
+				groceryListIDs = groceryListIDs.filter(groceryListID=> groceryListID !== id)
+				if(groceryListIDs.length === 0){
+					groceryListIDs[0] = ''
+				}
+				return restartDataStore()
+			}
+			return
+		default:
+			console.log('default')
+			return restartDataStore();
+	}
+  }
+
+
+
+  export async function restartDataStore(){
+    await DataStore.stop();
+    await DataStore.start();
+  }
+

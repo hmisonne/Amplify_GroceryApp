a4e35174e90c214ff880200c57838f87715dd6dd
hmisonne <helene.misonne@gmail.com>
2020-11-25 10:45:35 +0100

    feat: Divided listData into allProducts and toBuy only

--- screens/ProductList.js
+++ screens/ProductList.js
@@ -21,7 +21,7 @@ function ProductList(props) {
   useEffect(() => {
     dispatch(handleLoadProducts(groceryListID));
   }, []);
-  const { listData } = props;
+  const { allProducts, productsToBuy } = props;
 
   function deleteProduct(productID) {
     dispatch(handleDeleteProduct(productID));
@@ -43,7 +43,7 @@ function ProductList(props) {
         toggleProducts={toggleProducts}
       />
       <SwipeSectionList
-        listData={listData}
+        listData={visibleProducts? allProducts: productsToBuy}
         deleteProduct={(productID) => deleteProduct(productID)}
         navigateToEditProduct={(product) => navigateToEditProduct(product)}
         toggleProduct={(product) => toggleProduct(product)}
@@ -57,17 +57,26 @@ function mapStateToProps(state) {
   const { products } = state;
   const currCategories = new Set();
   products.forEach((product) => currCategories.add(product.category));
-  let currListCategories = Array.from(currCategories)
-    .map((cat) => ({
-      title: cat,
-      key: productCategory[cat].key,
-      data: products
-        .filter((product) => product.category === cat)
-        .map((product) => ({ ...product, key: product.id })),
-    }))
-    .sort((a, b) => a.key - b.key);
+  let currListCategories = Array.from(currCategories).map((cat) => ({
+    title: cat,
+    key: productCategory[cat].key,
+    data: products
+      .filter((product) => product.category === cat)
+      .map((product) => ({ ...product, key: product.id })),
+  })).sort((a,b)=>a.key-b.key);
 
-  return { listData: currListCategories };
+  const filteredProducts = products.filter((product) => product.toBuy === true)
+  const filteredCurrCategories = new Set();
+  filteredProducts.forEach((product) => filteredCurrCategories.add(product.category));
+  let currListCategoriesFiltered = Array.from(filteredCurrCategories).map((cat) => ({
+    title: cat,
+    key: productCategory[cat].key,
+    data: filteredProducts
+      .filter((product) => product.category === cat)
+      .map((product) => ({ ...product, key: product.id })),
+  })).sort((a,b)=>a.key-b.key);
+
+  return { allProducts: currListCategories, productsToBuy: currListCategoriesFiltered };
 }
 export default connect(mapStateToProps)(ProductList);
 

d9551ef9360615f0d08bb78cf9882312756dbec2
hmisonne <helene.misonne@gmail.com>
2020-12-08 19:14:44 +0100

    refactor: Updated action list with validation key

--- screens/ProductList.js
+++ screens/ProductList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
-import { FAB, Provider } from 'react-native-paper';
+import { FAB, Provider } from "react-native-paper";
 import { connect, useDispatch } from "react-redux";
 import { DataStore } from "aws-amplify";
 
@@ -17,47 +17,50 @@ import HeaderTab from "../components/HeaderTab";
 import SwipeSectionList from "../components/SwipeSectionList";
 import MenuOptions from "../components/MenuOptions";
 
-function ProductList({ navigation, route, allProducts, productsToBuy}) {
+function ProductList({ navigation, route, allProducts, productsToBuy }) {
   const dispatch = useDispatch();
   const [toBuyView, setToBuyView] = useState(true);
   function toggleToBuyView(bool) {
     return setToBuyView(bool);
   }
 
-  const { groceryList } = route.params
+  const { groceryList } = route.params;
   const groceryListID = groceryList.id;
 
   const actionsMenu = [
     {
       icon: "share-variant",
       title: "Share",
+      validationNeeded: false,
       onPress: (groceryList) =>
         navigation.push("ShareGroceryList", { groceryList: groceryList }),
     },
     {
-      icon: "checkbox-multiple-blank-circle-outline" ,
+      icon: "checkbox-multiple-blank-circle-outline",
       title: "Uncheck all items",
+      validationNeeded: true,
       onPress: (groceryList) =>
-        dispatch(handleUnCheckAllProducts(groceryList.id))
+        dispatch(handleUnCheckAllProducts(groceryList.id)),
     },
     {
-      icon: "delete-variant" ,
+      icon: "delete-variant",
       title: "Delete all items",
+      validationNeeded: true,
       onPress: (groceryList) =>
-        dispatch(handleDeleteAllProducts(groceryList.id))
+        dispatch(handleDeleteAllProducts(groceryList.id)),
     },
-
   ];
 
   React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <MenuOptions 
-          actionsMenu={actionsMenu} 
-          groceryList={groceryList} />
-      ),
-  }, [navigation, actionsMenu, groceryList]);
-})
+    navigation.setOptions(
+      {
+        headerRight: () => (
+          <MenuOptions actionsMenu={actionsMenu} groceryList={groceryList} />
+        ),
+      },
+      [navigation, actionsMenu, groceryList]
+    );
+  });
 
   useEffect(() => {
     dispatch(handleLoadProducts(groceryListID));
@@ -79,41 +82,48 @@ function ProductList({ navigation, route, allProducts, productsToBuy}) {
     return dispatch(handleToggleProduct(product));
   }
   async function toggleProductToBuy(product) {
-    return dispatch(handleToggleProduct(product, 'toBuy'));
+    return dispatch(handleToggleProduct(product, "toBuy"));
   }
 
   return (
     <Provider>
-    
-    <View style={styles.container}>
-      <HeaderTab
-        firstTabSelected={toBuyView}
-        switchToSecondTab={toggleToBuyView}
-      />
-      <SwipeSectionList
-        listData={toBuyView? productsToBuy : allProducts}
-        deleteProduct={toBuyView? 
-          (product) => toggleProductToBuy(product)
-          : (product) => deleteProduct(product.id)}
-        navigateToEditProduct={(product) => navigateToEditProduct(product)}
-        toggleProduct={toBuyView? (product) => toggleProduct(product): (product) => toggleProductToBuy(product)}
-        toBuyView={toBuyView}
-      />
-       <FAB
-        style={styles.fab}
-        icon="plus"
-        style={{
-          backgroundColor: mainColor,
-          position: 'absolute',
-          margin: 16,
-          right: 0,
-          bottom: 0,}
-        }
-        onPress={() => navigation.push("AddProduct", {
-          groceryListID: groceryListID,
-        })}
-      />
-    </View>
+      <View style={styles.container}>
+        <HeaderTab
+          firstTabSelected={toBuyView}
+          switchToSecondTab={toggleToBuyView}
+        />
+        <SwipeSectionList
+          listData={toBuyView ? productsToBuy : allProducts}
+          deleteProduct={
+            toBuyView
+              ? (product) => toggleProductToBuy(product)
+              : (product) => deleteProduct(product.id)
+          }
+          navigateToEditProduct={(product) => navigateToEditProduct(product)}
+          toggleProduct={
+            toBuyView
+              ? (product) => toggleProduct(product)
+              : (product) => toggleProductToBuy(product)
+          }
+          toBuyView={toBuyView}
+        />
+        <FAB
+          style={styles.fab}
+          icon="plus"
+          style={{
+            backgroundColor: mainColor,
+            position: "absolute",
+            margin: 16,
+            right: 0,
+            bottom: 0,
+          }}
+          onPress={() =>
+            navigation.push("AddProduct", {
+              groceryListID: groceryListID,
+            })
+          }
+        />
+      </View>
     </Provider>
   );
 }
@@ -123,26 +133,35 @@ function mapStateToProps(state) {
   // Format products for ALL product view
   const currCategories = new Set();
   products.forEach((product) => currCategories.add(product.category));
-  let currListCategories = Array.from(currCategories).map((cat) => ({
-    title: cat,
-    key: productCategory[cat].key,
-    data: products
-      .filter((product) => product.category === cat)
-      .map((product) => ({ ...product, key: product.id })),
-  })).sort((a,b)=>a.key-b.key);
-// Format products for TO BUY product view
-  const filteredProducts = products.filter((product) => product.toBuy === true)
+  let currListCategories = Array.from(currCategories)
+    .map((cat) => ({
+      title: cat,
+      key: productCategory[cat].key,
+      data: products
+        .filter((product) => product.category === cat)
+        .map((product) => ({ ...product, key: product.id })),
+    }))
+    .sort((a, b) => a.key - b.key);
+  // Format products for TO BUY product view
+  const filteredProducts = products.filter((product) => product.toBuy === true);
   const filteredCurrCategories = new Set();
-  filteredProducts.forEach((product) => filteredCurrCategories.add(product.category));
-  let currListCategoriesFiltered = Array.from(filteredCurrCategories).map((cat) => ({
-    title: cat,
-    key: productCategory[cat].key,
-    data: filteredProducts
-      .filter((product) => product.category === cat)
-      .map((product) => ({ ...product, key: product.id })),
-  })).sort((a,b)=>a.key-b.key);
+  filteredProducts.forEach((product) =>
+    filteredCurrCategories.add(product.category)
+  );
+  let currListCategoriesFiltered = Array.from(filteredCurrCategories)
+    .map((cat) => ({
+      title: cat,
+      key: productCategory[cat].key,
+      data: filteredProducts
+        .filter((product) => product.category === cat)
+        .map((product) => ({ ...product, key: product.id })),
+    }))
+    .sort((a, b) => a.key - b.key);
 
-  return { allProducts: currListCategories, productsToBuy: currListCategoriesFiltered };
+  return {
+    allProducts: currListCategories,
+    productsToBuy: currListCategoriesFiltered,
+  };
 }
 export default connect(mapStateToProps)(ProductList);
 

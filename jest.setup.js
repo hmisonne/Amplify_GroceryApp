4dcd38667fb0f7ca9c795044ee297e45eaca3f2d
hmisonne <helene.misonne@gmail.com>
2020-12-15 16:22:26 -0800

    test: Added package and config for jest to run without errors

---
+++ jest.setup.js
@@ -0,0 +1,8 @@
+import { NativeModules } from 'react-native';
+
+NativeModules.RNCNetInfo = {
+  getCurrentState: jest.fn(() => Promise.resolve()),
+  addListener: jest.fn(),
+  removeListeners: jest.fn()
+};
+

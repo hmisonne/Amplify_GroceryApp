911b4634b9b75a9a199aec1ea3a3c013fd374112
hmisonne <helene.misonne@gmail.com>
2020-12-17 07:04:41 -0800

    style: Added Vertical Separator and icons to header tab

--- components/HeaderTab.js
+++ components/HeaderTab.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
 import { secondaryColor, lightGrey, mainColor } from "../utils/helpers";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const HeaderTab = ({ firstTabSelected, switchToSecondTab }) => (
   <View style={styles.rowAligned}>
@@ -10,19 +11,44 @@ const HeaderTab = ({ firstTabSelected, switchToSecondTab }) => (
       disabled={firstTabSelected}
       onPress={() => switchToSecondTab(true)}
     >
+      <MaterialCommunityIcons
+        name="cart"
+        size={20}
+        color={firstTabSelected ? secondaryColor : lightGrey}
+        style={{ alignSelf: "center" }}
+      />
       <Text style={{ color: firstTabSelected ? secondaryColor : lightGrey }}>
         MY CART
       </Text>
     </TouchableOpacity>
-    <TouchableOpacity
+
+   
+
+    <View
+      style={{
+        height: 40,
+        width: 0.8 ,
+        backgroundColor: lightGrey,
+      }}
+    />
+
+<TouchableOpacity
       style={!firstTabSelected && styles.bottomLine}
       disabled={!firstTabSelected}
       onPress={() => switchToSecondTab(false)}
     >
-      <Text style={{ color: firstTabSelected ? lightGrey: secondaryColor }}>
-        SHOPPING HISTORY
+      <MaterialCommunityIcons
+        name="history"
+        size={20}
+        color={firstTabSelected ? lightGrey : secondaryColor}
+        style={{ alignSelf: "center" }}
+      />
+      <Text style={{ color: firstTabSelected ? lightGrey : secondaryColor }}>
+        HISTORY
       </Text>
     </TouchableOpacity>
+
+   
   </View>
 );
 
@@ -42,6 +68,6 @@ const styles = StyleSheet.create({
 });
 
 HeaderTab.propTypes = {
-  firstTabSelected: PropTypes.bool.isRequired, 
-  switchToSecondTab:  PropTypes.func.isRequired
+  firstTabSelected: PropTypes.bool.isRequired,
+  switchToSecondTab: PropTypes.func.isRequired,
 };

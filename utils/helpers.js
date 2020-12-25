172a2fe702b70e9244d92cae26265513f9494525
hmisonne <helene.misonne@gmail.com>
2020-12-25 11:28:35 -0800

    refactor: Updated formatSectionListData to sort alphabetically products

--- utils/helpers.js
+++ utils/helpers.js
@@ -107,7 +107,17 @@ export function formatSectionListData(products) {
       key: cat,
       data: products
         .filter((product) => product.category === cat)
-        .sort((a,b)=> a.name - b.name)
+        .sort((a, b) => {
+          let fa = a.name.toLowerCase(),
+              fb = b.name.toLowerCase();
+          if (fa < fb) {
+              return -1;
+          }
+          if (fa > fb) {
+              return 1;
+          }
+          return 0;
+      })
         .map((product) => ({ ...product, key: product.id })),
     }));
   return currListCategories;

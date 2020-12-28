33061e269540c0f293b23268056a7bb15a733448
hmisonne <helene.misonne@gmail.com>
2020-12-28 15:18:14 -0800

    test: Added test

---
+++ screens/__tests__/NewGroceryListForm.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "../../src/redux/store";
+import { render, fireEvent } from "@testing-library/react-native";
+import NewGroceryListForm from "../NewGroceryListForm";
+
+test("adds a new Grocery List when the button is pressed", () => {
+  const route = { params: {} };
+  const mockFn = jest.fn();
+  const navigation = { setOptions: mockFn };
+
+  const component = (
+    <Provider store={store}>
+      <NewGroceryListForm route={route} navigation={navigation} />
+    </Provider>
+  );
+
+  const { debug, getByText, getByTestId } = render(component);
+  const input = getByTestId("TextInput");
+  const button = getByText("Save");
+
+  fireEvent.changeText(input, "Test List");
+  expect(input).toBeTruthy();
+  fireEvent.press(button);
+  const userState = store.getState().user;
+  expect(userState.groceryLists.length).toEqual(1);
+});
+
+
+// Remove API call from handleCreateGroceryList function: redux/actions/groceryLists
+// export function handleCreateGroceryList(groceryList) {
+//     return (dispatch) => {
+//               dispatch(addGroceryListToUser(groceryList))
+//       }
+//   }
+
+// button on header not rendered with jest, move it to the return statement

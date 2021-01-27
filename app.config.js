cc506793bdc0730aad11d412b7c30e211aec3b2c
hmisonne <helene.misonne@gmail.com>
2021-01-27 08:34:14 -0800

    Added app.json config

--- app.config.js
+++ app.config.js
@@ -1,17 +1,50 @@
 export default {
-    hooks: {
-        "postPublish": [
-          {
-            "file": "sentry-expo/upload-sourcemaps",
-            "config": {
-              "organization": process.env.SENTRY_ORGANIZATION,
-              "project": "listbee",
-              "authToken": process.env.SENTRY_AUTH_TOKEN
-            }
-          }
-        ]
-      },
-    extra: {
-      sentryDSN: process.env.SENTRY_DSN,
+    "name": "ListBee",
+    "slug": "ListBee",
+    "version": "1.0.4",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
     },
+    "updates": {
+      "fallbackToCacheTimeout": 0
+    },
+    "assetBundlePatterns": [
+      "**/*"
+    ],
+    "ios": {
+      "supportsTablet": true,
+      "bundleIdentifier": "com.hmisonne.ListBee",
+      "buildNumber": "1.0.17"
+    },
+    "web": {
+      "favicon": "./assets/icon.png"
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      "package": "com.hmisonne.ListBee",
+      "versionCode": 32,
+      "permissions": []
+    },
+    "hooks": {
+      "postPublish": [
+        {
+          "file": "sentry-expo/upload-sourcemaps",
+          "config": {
+            "organization": process.env.SENTRY_ORGANIZATION,
+            "project": "listbee",
+            "authToken": process.env.SENTRY_AUTH_TOKEN
+          }
+        }
+      ]
+    },
+  "extra": {
+    sentryDSN: process.env.SENTRY_DSN,
+  },
   };
